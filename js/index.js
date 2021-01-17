278eb89c06b025233b2fcda413496113097e6e4c
itvitjok <vvitjok@ukr.net>
2021-01-17 13:04:01 +0200

    js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-alert(hello);
+// alert('hello');
+
+let message = 'Hello JS!';
+alert (message);
+
+let name = prompt('Enter your name');
+alert(name);
+
+let age = Number(prompt('Enter your age'));
+alert('Your age is' + age);
+
+age = age + 1;
+alert('Next year you will be ' + age);
+
+if (age >= 18) {
+    alert('You are adult');
+} else {
+    alert('You are underage');
+}
+
+function add(a, b) {
+    const result = a + b;
+    return result;
+}
+
